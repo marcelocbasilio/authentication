68b643223f3f2846c7f0f1fe6e25c018bc374d91
Marcelo Basílio <marcelocbasilio@gmail.com>
2025-09-21 15:53:40 -0300

    Insert BCrypt in the project.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,24 +1,24 @@
 -- Seed in the table User
-INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('João Silva', 'joao.silva@email.com', '(11) 91234-5678', '1990-04-15', 'senha123');
-INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Maria Oliveira', 'maria.oliveira@email.com', '(21) 98765-4321', '1985-08-22', 'senha123');
-INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Carlos Souza', 'carlos.souza@email.com', '(31) 99876-5432', '1992-12-01', 'senha123');
-INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Ana Costa', 'ana.costa@email.com', '(41) 92345-6789', '1995-03-10', 'senha123');
-INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Lucas Pereira', 'lucas.pereira@email.com', '(51) 91987-6543', '1988-09-25', 'senha123');
-INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Fernanda Santos', 'fernanda.santos@email.com', '(61) 93123-4567', '1991-11-30', 'senha123');
-INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Rafael Almeida', 'rafael.almeida@email.com', '(71) 96789-0123', '1987-06-17', 'senha123');
-INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Beatriz Rocha', 'beatriz.rocha@email.com', '(81) 92834-1122', '1993-01-28', 'senha123');
-INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Ricardo Lima', 'ricardo.lima@email.com', '(91) 93984-2233', '1986-02-09', 'senha123');
-INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Paula Martins', 'paula.martins@email.com', '(51) 91834-7766', '1990-07-05', 'senha123');
-INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Gabriel Ferreira', 'gabriel.ferreira@email.com', '(61) 94455-6677', '1994-10-11', 'senha123');
-INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Juliana Pereira', 'juliana.pereira@email.com', '(41) 93677-8899', '1989-12-13', 'senha123');
-INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Daniela Costa', 'daniela.costa@email.com', '(11) 93122-3344', '1992-05-16', 'senha123');
-INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Mateus Souza', 'mateus.souza@email.com', '(21) 96783-0011', '1996-07-25', 'senha123');
-INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Isabela Martins', 'isabela.martins@email.com', '(61) 92984-5566', '1990-11-02', 'senha123');
-INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Felipe Rocha', 'felipe.rocha@email.com', '(51) 98342-7766', '1987-03-18', 'senha123');
-INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Carla Almeida', 'carla.almeida@email.com', '(71) 96711-2233', '1995-08-07', 'senha123');
-INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Eduardo Lima', 'eduardo.lima@email.com', '(41) 91234-5567', '1993-01-30', 'senha123');
-INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Larissa Oliveira', 'larissa.oliveira@email.com', '(11) 96432-7788', '1994-04-12', 'senha123');
-INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Vitor Santos', 'vitor.santos@email.com', '(21) 98876-4455', '1989-11-09', 'senha123');
+INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('João Silva', 'joao.silva@email.com', '(11) 91234-5678', '1990-04-15', '$2a$10$.mGgzbua2c6EITuzxRvtNORZByqbbhFGKg.sUiZHo3osf94dwttTu');
+INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Maria Oliveira', 'maria.oliveira@email.com', '(21) 98765-4321', '1985-08-22', '$2a$10$.mGgzbua2c6EITuzxRvtNORZByqbbhFGKg.sUiZHo3osf94dwttTu');
+INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Carlos Souza', 'carlos.souza@email.com', '(31) 99876-5432', '1992-12-01', '$2a$10$.mGgzbua2c6EITuzxRvtNORZByqbbhFGKg.sUiZHo3osf94dwttTu');
+INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Ana Costa', 'ana.costa@email.com', '(41) 92345-6789', '1995-03-10', '$2a$10$.mGgzbua2c6EITuzxRvtNORZByqbbhFGKg.sUiZHo3osf94dwttTu');
+INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Lucas Pereira', 'lucas.pereira@email.com', '(51) 91987-6543', '1988-09-25', '$2a$10$.mGgzbua2c6EITuzxRvtNORZByqbbhFGKg.sUiZHo3osf94dwttTu');
+INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Fernanda Santos', 'fernanda.santos@email.com', '(61) 93123-4567', '1991-11-30', '$2a$10$.mGgzbua2c6EITuzxRvtNORZByqbbhFGKg.sUiZHo3osf94dwttTu');
+INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Rafael Almeida', 'rafael.almeida@email.com', '(71) 96789-0123', '1987-06-17', '$2a$10$.mGgzbua2c6EITuzxRvtNORZByqbbhFGKg.sUiZHo3osf94dwttTu');
+INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Beatriz Rocha', 'beatriz.rocha@email.com', '(81) 92834-1122', '1993-01-28', '$2a$10$.mGgzbua2c6EITuzxRvtNORZByqbbhFGKg.sUiZHo3osf94dwttTu');
+INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Ricardo Lima', 'ricardo.lima@email.com', '(91) 93984-2233', '1986-02-09', '$2a$10$.mGgzbua2c6EITuzxRvtNORZByqbbhFGKg.sUiZHo3osf94dwttTu');
+INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Paula Martins', 'paula.martins@email.com', '(51) 91834-7766', '1990-07-05', '$2a$10$.mGgzbua2c6EITuzxRvtNORZByqbbhFGKg.sUiZHo3osf94dwttTu');
+INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Gabriel Ferreira', 'gabriel.ferreira@email.com', '(61) 94455-6677', '1994-10-11', '$2a$10$.mGgzbua2c6EITuzxRvtNORZByqbbhFGKg.sUiZHo3osf94dwttTu');
+INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Juliana Pereira', 'juliana.pereira@email.com', '(41) 93677-8899', '1989-12-13', '$2a$10$.mGgzbua2c6EITuzxRvtNORZByqbbhFGKg.sUiZHo3osf94dwttTu');
+INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Daniela Costa', 'daniela.costa@email.com', '(11) 93122-3344', '1992-05-16', '$2a$10$.mGgzbua2c6EITuzxRvtNORZByqbbhFGKg.sUiZHo3osf94dwttTu');
+INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Mateus Souza', 'mateus.souza@email.com', '(21) 96783-0011', '1996-07-25', '$2a$10$.mGgzbua2c6EITuzxRvtNORZByqbbhFGKg.sUiZHo3osf94dwttTu');
+INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Isabela Martins', 'isabela.martins@email.com', '(61) 92984-5566', '1990-11-02', '$2a$10$.mGgzbua2c6EITuzxRvtNORZByqbbhFGKg.sUiZHo3osf94dwttTu');
+INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Felipe Rocha', 'felipe.rocha@email.com', '(51) 98342-7766', '1987-03-18', '$2a$10$.mGgzbua2c6EITuzxRvtNORZByqbbhFGKg.sUiZHo3osf94dwttTu');
+INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Carla Almeida', 'carla.almeida@email.com', '(71) 96711-2233', '1995-08-07', '$2a$10$.mGgzbua2c6EITuzxRvtNORZByqbbhFGKg.sUiZHo3osf94dwttTu');
+INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Eduardo Lima', 'eduardo.lima@email.com', '(41) 91234-5567', '1993-01-30', '$2a$10$.mGgzbua2c6EITuzxRvtNORZByqbbhFGKg.sUiZHo3osf94dwttTu');
+INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Larissa Oliveira', 'larissa.oliveira@email.com', '(11) 96432-7788', '1994-04-12', '$2a$10$.mGgzbua2c6EITuzxRvtNORZByqbbhFGKg.sUiZHo3osf94dwttTu');
+INSERT INTO tb_user (name, email, phone, birth_date, password) VALUES ('Vitor Santos', 'vitor.santos@email.com', '(21) 98876-4455', '1989-11-09', '$2a$10$.mGgzbua2c6EITuzxRvtNORZByqbbhFGKg.sUiZHo3osf94dwttTu');
 
 -- Seed in the table Product
 INSERT INTO tb_product (name) values ('TV');
