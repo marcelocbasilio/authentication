17220da7a8b413247e24972c162fe6d2c9e5bd97
Marcelo Basílio <marcelocbasilio@gmail.com>
2025-09-21 09:17:00 -0300

    Create the file import.sql with mock.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+-- Insert in the table User
+INSERT INTO tb_user (name, email, password) values ('Marcelo', 'marcelocbasilio@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) values ('Ramily', 'ramilycarvalho@gmail.com', '123456');
+
+-- Insert in the table Product
+INSERT INTO tb_product (name) values ('TV');
+INSERT INTO tb_product (name) values ('Computer');
